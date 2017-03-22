2bc6ced7379c13c5ccc9ecd2ac2560154d21b5fe
Henrik Qvigstad <henqvi@kantega.no>
2017-03-22 07:07:11 +0100

    remove opera

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 backupdir=$dir/dotfiles_backup             # old dotfiles backup directory
-files="bashrc vimrc vim opera profile pythonrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim profile pythonrc"    # list of files/folders to symlink in homedir
 
 ##########
 
